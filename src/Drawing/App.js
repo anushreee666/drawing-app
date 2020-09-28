a26075185d453146f0af8501ae752b55fd49b668
anushree <anushree8111999@gmail.com>
2020-09-28 17:08:03 +0530

    first commit

--- src/Drawing/App.js
+++ src/Drawing/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-
-import SignIn from "../LoginModule/auth/SignIn";
-import SignUp from "../LoginModule/auth/SignUp";
 import Home from "./Components/Home";
 
 function App() {
@@ -11,8 +8,6 @@ function App() {
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={Home}></Route>
-				<Route path="/SignIn" component={SignIn}></Route>
-				<Route path="/SignUp" component={SignUp}></Route>
 			</Switch>
 		</BrowserRouter>
 	);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/*import App from "./Drawing/App";
-import App from "./PasswordCaptcha/App";
-import App from "./Quiz/App"; 
-import App from "./Recipe/App";
-import App from "./Weather/App";
-import App from "./CountdownTimer/App";*/
-import App from "./GithubProfile/App";
-/*import App from "./Notes/App"; 
-import App from "./Movie/App"; 
-import App from "./Todo/App";*/
+import App from "./Drawing/App";
 
 ReactDOM.render(
 	<React.StrictMode>
